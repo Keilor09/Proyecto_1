fb4c7c4140f33f4817f37bda994b21807b910525
Keilor Aguilar <keilor2019@gmail.com>
2023-10-22 10:24:41 -0600

    Clase Lista y Nodo

--- Jugador.cpp
+++ Jugador.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Jugador.h"
+
+Jugador::Jugador(const string &id, const string &nombre, float dineroDisponible) : id(id), nombre(nombre),
+                                                                                   dineroDisponible(dineroDisponible) {}
+
+
+Jugador::Jugador() {
+    id = "";
+    nombre = "";
+    dineroDisponible = 0.0;
+}
+
+
+Jugador::~Jugador() {
+
+}
+
+const string &Jugador::getId() const {
+    return id;
+}
+
+void Jugador::setId(const string &id) {
+    Jugador::id = id;
+}
+
+const string &Jugador::getNombre() const {
+    return nombre;
+}
+
+void Jugador::setNombre(const string &nombre) {
+    Jugador::nombre = nombre;
+}
+
+float Jugador::getDineroDisponible() const {
+    return dineroDisponible;
+}
+
+void Jugador::setDineroDisponible(float dineroDisponible) {
+    Jugador::dineroDisponible = dineroDisponible;
+}
--- Jugador.h
+++ Jugador.h
@@ -6,10 +6,32 @@
 #define PROYECTO_1_JUGADOR_H
 
 #include <iostream>
-
+#include "Lista.h"
 using namespace std;
 class Jugador {
     private:
+        string id;
+        string nombre;
+        float dineroDisponible;
+        //Lista<Vehiculo> vehiculosDisponibles;
+    public:
+        Jugador(const string &id, const string &nombre, float dineroDisponible);
+
+        Jugador();
+
+        virtual ~Jugador();
+
+        const string &getId() const;
+
+        void setId(const string &id);
+
+        const string &getNombre() const;
+
+        void setNombre(const string &nombre);
+
+        float getDineroDisponible() const;
+
+        void setDineroDisponible(float dineroDisponible);
 
 };
 
