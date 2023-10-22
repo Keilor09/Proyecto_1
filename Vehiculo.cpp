09d09c3cce9fe3ccc2df23c220d2c6b58788190e
Keilor Aguilar <keilor2019@gmail.com>
2023-10-22 11:20:12 -0600

    Clase vehiculo heredadad de item

--- Vehiculo.cpp
+++ Vehiculo.cpp
@@ -2,4 +2,4 @@
 // Created by keilo on 10/22/2023.
 //
 
-#include "Tienda.h"
+#include "Vehiculo.h"
---
+++ Vehiculo.h
@@ -0,0 +1,16 @@
+//
+// Created by keilo on 10/22/2023.
+//
+
+#ifndef PROYECTO_1_VEHICULO_H
+#define PROYECTO_1_VEHICULO_H
+
+#include "Item.h"
+
+class Vehiculo : public Item {
+
+
+};
+
+
+#endif //PROYECTO_1_VEHICULO_H
